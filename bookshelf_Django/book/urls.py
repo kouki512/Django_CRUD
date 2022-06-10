059c32da9dd62adc2a41c97972cc5cd642a58b29
kouki512 <ht20a008@oecu.jp>
2022-06-10 17:54:48 +0900

    [Finish]CRUD APP

--- bookshelf_Django/book/urls.py
+++ bookshelf_Django/book/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('book/', views.ListBookView.as_view()) ,# ClassBasedViewsの呼び出し方
-
-    path('book/<int:pk>/detail/', views.DetailBookView.as_view()) ,
+    path('book/', views.ListBookView.as_view(),name = 'list-book') ,# ClassBasedViewsの呼び出し方
+    path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'),
+    path('book/create/', views.CreateBookView.as_view(), name='create-book') ,
+    path('book/<int:pk>/delete/', views.DeleteBookView.as_view(),name='delete-book'),
+    path('book/<int:pk>/update/', views.UpdateBookView.as_view(),name='update-book'),
 ]
--- bookshelf_Django/book/views.py
+++ bookshelf_Django/book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import( ListView, DetailView,CreateView,DeleteView,UpdateView,) #テンプレートの利用する際はここも追加する必要あり。
+from django.urls import reverse_lazy
 from .models import  Book 
 
 class ListBookView(ListView):
@@ -10,3 +11,20 @@ class DetailBookView(DetailView):
     template_name = 'book/book_detail.html'
     model = Book
 # Create your views here.
+
+class CreateBookView(CreateView):
+    template_name = 'book/book_create.html'
+    model = Book
+    fields = {'title','text','category'}
+    success_url = reverse_lazy('list-book')
+
+class DeleteBookView(DeleteView):
+    templete_name = 'book/book_confirm_delete.html'
+    model = Book
+    success_url = reverse_lazy('list-book')
+
+class UpdateBookView(UpdateView):
+    model = Book
+    fields = {'title','text','category'}
+    template_name = 'book/book_update.html'
+    success_url = reverse_lazy('list-book')
